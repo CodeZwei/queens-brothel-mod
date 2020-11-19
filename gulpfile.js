752018aea458be3e3b14267405d74ad445646edc
CodeZwei <brandonzwei@gmail.com>
2020-11-19 13:33:38 -0800

    Adjusted gulp tasks to name the zip based on id and version

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,30 @@ const gulp = require('gulp');
 const del = require('del');
 const zip = require('gulp-zip');
 
+const config = require('./build/config.json')
+
 gulp.task('delDist', () => {
     return del(['dist']);
 });
 
+gulp.task('delTemp', () => {
+    return del(['temp']);
+});
+
 gulp.task('moveAssets', () => {
     return gulp.src('public/assets/**/*')
-        .pipe(gulp.dest('dist/assets'));
+        .pipe(gulp.dest('temp/assets'));
 });
 
 gulp.task('moveConfigAndMod', () => {
     return gulp.src(['build/config.json', 'public/js/mod.js'])
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('temp'));
 });
 
 gulp.task('zipFiles', () => {
-    return gulp.src('dist/**')
-        .pipe(zip('mod.zip'))
+    return gulp.src('temp/**')
+        .pipe(zip(`${config.id}-${config.version}`))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('deleteFiles', () => {
-    return del(['dist/**/*', '!dist/mod.zip']);
-});
-
-gulp.task('default', gulp.series('delDist', 'moveAssets', 'moveConfigAndMod', 'zipFiles', 'deleteFiles'));
+gulp.task('default', gulp.series('delDist', 'moveAssets', 'moveConfigAndMod', 'zipFiles', 'delTemp'));
